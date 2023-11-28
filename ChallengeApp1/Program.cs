3fef8515323289395aae687532622c4ad8d940f1
Henryk Natanek <henrynatanek@interia.pl>
2023-11-28 19:00:30 +0100

    Zadanie domowe dzien 4

--- ChallengeApp1/Program.cs
+++ ChallengeApp1/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// "kobieta poniżej 30 lat"
+// "Ewa lat 33"
+// "Niepełnoletni męszczyzna"
+
+bool isWommen = true;
+int age = 18;
+string name = "Ewa;";
+
+if (isWommen == true && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33 )
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if (isWommen == false && age < 18)
+{
+    Console.WriteLine("Niepełnoletni męszczyzna");
+}
+else
+{
+    Console.WriteLine("Ktoś inny");
+}
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
