17bc96082f3e18fd9b9c6b8d8f334aa1a6dd55c5
Henryk Natanek <henrynatanek@interia.pl>
2024-02-06 18:31:33 +0100

    zadanie dzien5

--- ChallengeApp1/Program.cs
+++ ChallengeApp1/Program.cs
@@ -1,33 +1,73 @@
-﻿// "kobieta poniżej 30 lat"
-// "Ewa lat 33"
-// "Niepełnoletni męszczyzna"
+﻿int  number = 19168;
+string numberAsString = number. ToString();
+char[] letters = numberAsString.ToArray();
 
-bool isWommen = true;
-int age = 18;
-string name = "Ewa;";
+int count0 = 0;
+int count1 = 0;
+int count2 = 0;
+int count3 = 0;
+int count4 = 0;
+int count5 = 0;
+int count6 = 0;
+int count7 = 0;
+int count8 = 0;
+int count9 = 0;
 
-if (isWommen == true && age < 30)
+foreach (char letter in letters)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (letter == '0')
+    {
+        count0++;
+    }
+    else if (letter == '1')
+    {
+        count1++;
+    }
+    else if (letter == '2')
+    {
+        count2++;
+    }
+    else if (letter == '3')
+    {
+        count3++;
+    }
+    else if (letter == '4')
+    {
+        count4++;
+    }
+    else if (letter == '5')
+    {
+        count5++;
+    }
+    else if (letter == '6')
+    {
+        count6++;
+    }
+    else if (letter == '7')
+    {
+        count7++;
+    }
+    else if (letter == '8')
+    {
+        count8++;
+    }
+    else if (letter == '9')
+    {
+        count9++;
+    }
 }
-else if (name == "Ewa" && age == 33 )
-{
-    Console.WriteLine("Ewa lat 33");
-}
-else if (isWommen == false && age < 18)
-{
-    Console.WriteLine("Niepełnoletni męszczyzna");
-}
-else
-{
-    Console.WriteLine("Ktoś inny");
-}
-     
 
-
-
-
-
+Console.WriteLine("Wynik dla liczby:" + number);
+Console.WriteLine("0 => " + count0);
+Console.WriteLine("1 => " + count1);
+Console.WriteLine("2 => " + count2);
+Console.WriteLine("3 => " + count3);
+Console.WriteLine("4 => " + count4);
+Console.WriteLine("5 => " + count5);
+Console.WriteLine("6 => " + count6);
+Console.WriteLine("7 => " + count7);
+Console.WriteLine("8 => " + count8);
+Console.WriteLine("9 => " + count9);
 
 
 
